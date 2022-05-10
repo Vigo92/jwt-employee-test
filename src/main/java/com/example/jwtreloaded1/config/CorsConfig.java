48863093632a1cfd1ce6844d86d189cb2c5c1737
Ayodeji <afuwapeayodeji2018@gmail.com>
2022-05-10 06:37:44 +0100

    cors

--- src/main/java/com/example/jwtreloaded1/config/CorsConfig.java
+++ src/main/java/com/example/jwtreloaded1/config/CorsConfig.java
@@ -13,6 +13,6 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedHeaders("*")
-                .allowedOrigins("*");
+                .allowedOrigins("http://localhost:8080");
     }
 }
