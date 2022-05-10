59fe549a0f26e77f5aec5b7918319a7b095364b7
Ayodeji <afuwapeayodeji2018@gmail.com>
2022-05-10 06:45:31 +0100

    sss

--- src/main/java/com/example/jwtreloaded1/controller/EmployeeController.java
+++ src/main/java/com/example/jwtreloaded1/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.PermitAll;
 import javax.validation.Valid;
 
 
@@ -51,6 +52,7 @@ public class EmployeeController {
     }
 
 
+    @PermitAll
     @GetMapping("/welcome")
     public String Welcome() {
         return "welcome to the employee managament app";
