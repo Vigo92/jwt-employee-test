9b82c8add77d5ba702c290a38afa4f7c82c5693a
Ayodeji <afuwapeayodeji2018@gmail.com>
2022-05-11 19:51:50 +0100

    ss

--- src/main/java/com/example/jwtreloaded1/config/ProjectConfig.java
+++ src/main/java/com/example/jwtreloaded1/config/ProjectConfig.java
@@ -6,11 +6,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 
 @Configuration
-public class ProjectConfig {
+public class ProjectConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*");
+    }
+
 
 
     @Bean
@@ -18,13 +27,5 @@ public class ProjectConfig {
         return new ModelMapper();
     }
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+    
 }
